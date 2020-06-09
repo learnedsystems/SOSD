4aed89040c3494dc3c4232503dd6f2612375aa58
Ryan Marcus <ryan@ryanmarc.us>
2020-06-08 21:32:23 -0400

    updated RMI
    Use 8 threads, removed unused function
    
    Updated RMI

--- scripts/build_rmis.sh
+++ scripts/build_rmis.sh
@@ -3,22 +3,6 @@ git submodule update --init --recursive
 
 mkdir -p rmi_data
 
-function build_rmi() {
-    DATA_NAME=$1
-    HEADER1_NAME=${DATA_NAME}_rmi.h
-    HEADER2_NAME=${DATA_NAME}_rmi_data.h
-    C_NAME=${DATA_NAME}_rmi.cpp
-    HEADER_PATH=competitors/rmi/$HEADER1_NAME
-
-    shift 1
-    if [ ! -f $HEADER_PATH ]; then
-        echo "Building RMI for $DATA_NAME ($INCR)"
-        RUST_BACKTRACE=1 RUST_LOG=trace RMI/target/release/rmi data/$DATA_NAME ${DATA_NAME}_rmi $@ -d rmi_data/ -e
-        mv $HEADER1_NAME competitors/rmi/
-        mv $HEADER2_NAME competitors/rmi/
-        mv $C_NAME competitors/rmi/
-    fi
-}
 
 function build_rmi_set() {
     DATA_NAME=$1
@@ -28,7 +12,7 @@ function build_rmi_set() {
     shift 1
     if [ ! -f $HEADER_PATH ]; then
         echo "Building RMI set for $DATA_NAME"
-        RMI/target/release/rmi data/$DATA_NAME --param-grid ${JSON_PATH} -d rmi_data/
+        RMI/target/release/rmi data/$DATA_NAME --param-grid ${JSON_PATH} -d rmi_data/ --threads 8
         mv ${DATA_NAME}_* competitors/rmi/
     fi
 }
