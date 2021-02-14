2314052aac23b6cec184c921956ce74bac5e7d35
Allen Huang <allen.huang@doc.ai>
2021-02-14 10:48:49 -0800

    Added synthetic files to benchmark

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -15,19 +15,27 @@ function generate_lookups() {
     [ -f ../data/$1_equality_lookups_1M ] || ./generate ../data/$1 1000000
 }
 
-generate_lookups osm_cellids_200M_uint64
-generate_lookups osm_cellids_400M_uint64
-generate_lookups osm_cellids_600M_uint64
-generate_lookups osm_cellids_800M_uint64
+echo "Generating queries..."
+generate_lookups normal_200M_uint32
+generate_lookups normal_200M_uint64
 
+echo "(stand by, this one takes a while...)"
+generate_lookups lognormal_200M_uint32
+generate_lookups lognormal_200M_uint64
+
+generate_lookups uniform_dense_200M_uint32
+generate_lookups uniform_dense_200M_uint64
+
+generate_lookups uniform_sparse_200M_uint32
+generate_lookups uniform_sparse_200M_uint64
+
+generate_lookups osm_cellids_200M_uint64
 generate_lookups wiki_ts_200M_uint64
 
 generate_lookups books_200M_uint32
 generate_lookups books_200M_uint64
-generate_lookups books_400M_uint64
-generate_lookups books_600M_uint64
-generate_lookups books_800M_uint64
 
 generate_lookups fb_200M_uint64
+generate_lookups fb_200M_uint32
 
 
