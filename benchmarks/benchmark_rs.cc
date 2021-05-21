8a52c3ab1d37fa21ff09b3bf1fdcfa6caf983c0f
Andreas Kipf <kipf@mit.edu>
2021-05-21 10:49:08 +0000

    Exit RS gracefully

--- benchmarks/benchmark_rs.cc
+++ benchmarks/benchmark_rs.cc
@@ -7,34 +7,34 @@
 template <template <typename> typename Searcher>
 void benchmark_32_rs(sosd::Benchmark<uint32_t, Searcher>& benchmark,
                      bool pareto) {
-  benchmark.template Run<RS<uint32_t, 1>>();
+  benchmark.template Run<RS<uint32_t, 10>>();
   if (pareto) {
-    benchmark.template Run<RS<uint32_t, 2>>();
-    benchmark.template Run<RS<uint32_t, 3>>();
-    benchmark.template Run<RS<uint32_t, 4>>();
-    benchmark.template Run<RS<uint32_t, 5>>();
-    benchmark.template Run<RS<uint32_t, 6>>();
-    benchmark.template Run<RS<uint32_t, 7>>();
-    benchmark.template Run<RS<uint32_t, 8>>();
     benchmark.template Run<RS<uint32_t, 9>>();
-    benchmark.template Run<RS<uint32_t, 10>>();
+    benchmark.template Run<RS<uint32_t, 8>>();
+    benchmark.template Run<RS<uint32_t, 7>>();
+    benchmark.template Run<RS<uint32_t, 6>>();
+    benchmark.template Run<RS<uint32_t, 5>>();
+    benchmark.template Run<RS<uint32_t, 4>>();
+    benchmark.template Run<RS<uint32_t, 3>>();
+    benchmark.template Run<RS<uint32_t, 2>>();
+    benchmark.template Run<RS<uint32_t, 1>>();
   }
 }
 
 template <template <typename> typename Searcher>
 void benchmark_64_rs(sosd::Benchmark<uint64_t, Searcher>& benchmark,
                      bool pareto) {
-  benchmark.template Run<RS<uint64_t, 1>>();
+  benchmark.template Run<RS<uint64_t, 10>>();
   if (pareto) {
-    benchmark.template Run<RS<uint64_t, 2>>();
-    benchmark.template Run<RS<uint64_t, 3>>();
-    benchmark.template Run<RS<uint64_t, 4>>();
-    benchmark.template Run<RS<uint64_t, 5>>();
-    benchmark.template Run<RS<uint64_t, 6>>();
-    benchmark.template Run<RS<uint64_t, 7>>();
-    benchmark.template Run<RS<uint64_t, 8>>();
     benchmark.template Run<RS<uint64_t, 9>>();
-    benchmark.template Run<RS<uint64_t, 10>>();
+    benchmark.template Run<RS<uint64_t, 8>>();
+    benchmark.template Run<RS<uint64_t, 7>>();
+    benchmark.template Run<RS<uint64_t, 6>>();
+    benchmark.template Run<RS<uint64_t, 5>>();
+    benchmark.template Run<RS<uint64_t, 4>>();
+    benchmark.template Run<RS<uint64_t, 3>>();
+    benchmark.template Run<RS<uint64_t, 2>>();
+    benchmark.template Run<RS<uint64_t, 1>>();
   }
 }
 
