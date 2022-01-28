e4364b5f5a567795ad1b2428845d290466acb8d0
Allen <huanga@mit.edu>
2022-01-27 22:03:17 -0500

    Code reformat

--- benchmarks/benchmark_rmi_alt.cc
+++ benchmarks/benchmark_rmi_alt.cc
@@ -2,37 +2,69 @@
 
 #include "benchmark.h"
 #include "common.h"
-#include "competitors/rmi_alt.h"
 #include "competitors/analysis-rmi/include/rmi/models.hpp"
 #include "competitors/analysis-rmi/include/rmi/rmi.hpp"
+#include "competitors/rmi_alt.h"
 
 template <template <typename> typename Searcher>
 void benchmark_32_rmi_alt(sosd::Benchmark<uint32_t, Searcher>& benchmark,
                           bool pareto) {
-    benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 128, 1>>();
-    if (pareto) {
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 256, 2>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 512, 3>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 1024, 4>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 2048, 5>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 4096, 6>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 8192, 7>>();
-      benchmark.template Run<RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 16384, 8>>();
-    }
+  benchmark.template Run<
+      RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                   rmi::RmiLAbs, 128, 1>>();
+  if (pareto) {
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 256, 2>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 512, 3>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 1024, 4>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 2048, 5>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 4096, 6>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 8192, 7>>();
+    benchmark.template Run<
+        RMIAlternate<uint32_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 16384, 8>>();
+  }
 }
 
 template <template <typename> typename Searcher>
 void benchmark_64_rmi_alt(sosd::Benchmark<uint64_t, Searcher>& benchmark,
                           bool pareto) {
-  benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 128, 1>>();
+  benchmark.template Run<
+      RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                   rmi::RmiLAbs, 128, 1>>();
   if (pareto) {
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 256, 2>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 512, 3>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 1024, 4>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 2048, 5>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 4096, 6>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 8192, 7>>();
-    benchmark.template Run<RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression, rmi::RmiLAbs, 16384, 8>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 256, 2>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 512, 3>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 1024, 4>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 2048, 5>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 4096, 6>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 8192, 7>>();
+    benchmark.template Run<
+        RMIAlternate<uint64_t, rmi::LinearSpline, rmi::LinearRegression,
+                     rmi::RmiLAbs, 16384, 8>>();
   }
 }
 
--- competitors/rmi_alt.h
+++ competitors/rmi_alt.h
@@ -1,14 +1,16 @@
 #pragma once
 
-#include <utility>
 #include <memory>
+#include <utility>
 
-#include "./analysis-rmi/include/rmi/rmi.hpp"
 #include "./analysis-rmi/include/rmi/models.hpp"
+#include "./analysis-rmi/include/rmi/rmi.hpp"
 #include "base.h"
 
 // Alternate implementation of RMI as described by Maltry and Dittrich
-template <class KeyType, typename Layer1, typename Layer2, template <typename...> typename RMIType, size_t layer2_size, uint32_t variant_num>
+template <class KeyType, typename Layer1, typename Layer2,
+          template <typename...> typename RMIType, size_t layer2_size,
+          uint32_t variant_num>
 class RMIAlternate : public Competitor {
  public:
   uint64_t Build(const std::vector<KeyValue<KeyType>>& data) {
@@ -18,20 +20,16 @@ class RMIAlternate : public Competitor {
       loading_data.push_back(itm.key);
     }
 
-    return util::timing(
-        [&] {
-          auto new_rmi_ptr = std::make_unique<RMIType<KeyType, Layer1, Layer2>>(loading_data, layer2_size);
-          rmi_ = std::move(new_rmi_ptr);
-        });
+    return util::timing([&] {
+      auto new_rmi_ptr = std::make_unique<RMIType<KeyType, Layer1, Layer2>>(
+          loading_data, layer2_size);
+      rmi_ = std::move(new_rmi_ptr);
+    });
   }
 
   std::string name() const { return "RMIAlternate"; }
 
-  std::size_t size() const {
-    return rmi_->size_in_bytes();
-//    RMIType<KeyType, Layer1, Layer2>& ref = const_cast<RMIType<KeyType, Layer1, Layer2>&>(rmi_);
-//    return ref.size_in_bytes();
-  }
+  std::size_t size() const { return rmi_->size_in_bytes(); }
 
   SearchBound EqualityLookup(const KeyType lookup_key) const {
     auto it = rmi_->search(lookup_key);
