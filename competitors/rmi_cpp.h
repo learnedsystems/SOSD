ab6a66298c6ed03fd3842718ec8d3250ffb8f579
Allen <huanga@mit.edu>
2022-04-06 15:51:06 -0400

    RMI CPP updates

--- competitors/rmi_cpp.h
+++ competitors/rmi_cpp.h
@@ -5,7 +5,6 @@
 
 #include "./analysis-rmi/include/rmi/models.hpp"
 #include "./analysis-rmi/include/rmi/rmi.hpp"
-#include "./analysis-rmi/include/rmi/builder.hpp"
 #include "base.h"
 
 // Alternate implementation of RMI in C++
